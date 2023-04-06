a52cb1bae0a82526325feadc557f5c7c57d55048
Barry Moorer <123269095+BarryMoorer@users.noreply.github.com>
2023-04-06 10:42:28 -0400

    menu updates

--- script/script.js
+++ script/script.js
@@ -1,6 +1,32 @@
 "use strict";
+//$(document).ready(function() {
+ const menuButton = document.getElementsByClassName("mobile-menu")[0]
+const menuList = document.getElementsByClassName('menu-bar')[0]
 
-$(document).ready(function() {
-    
+menuButton.addEventListener('click', () => {
+    menuList.classList.toggle('active')
+    console.log('this one is working')
 })
 
+ //  $("#mobile-menu").click(
+      //  function () {
+    //    $(".menu-bar").css("display", "none")
+  //      $(this).toggleClass("active");
+    
+   // var menuList = document.querySelector(".menu_horizontal");
+
+   // menuList.classList.add(":active");
+    
+   // console.log('button clicked!')
+
+//})
+
+//})
+
+
+
+/*
+function menuClickHandler() {
+    $(".menu-bar").css("display", "none")
+    $(this).toggleClass("active");
+}*/
